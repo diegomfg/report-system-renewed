8524139ff93a095889357a2963c6f20e1624f2c4
Diego <diegoamatheus@gmail.com>
2022-01-08 11:04:29 -0500

    .

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,9 @@
 const Mongoose = require("mongoose");
 const connection_url =
     process.env.local_db || "mongodb://localhost:27017/report_system_node";
-
+/**
+ * @todo Research logging to files
+ */
 module.exports = () => {
         Mongoose.connect(connection_url)
         .then(()=> console.log("database connection ok"))
--- models/Response.js
+++ models/Response.js
@@ -1,11 +1,3 @@
-/**
- * @TODO What to do here
- * 
- * Create response model for res.send(new OkResponse() | new ErrorResponse())
- * 
- * Check the type Success|Error
- */
-
 class Response{
     constructor(type, data){
         this.type = type;
