6e5f929e9a0071439e96f352e80aefeff85cdf3a
Diego Matheus <diegoamatheus@gmail.com>
2022-07-07 12:24:24 -0400

    add navbar

--- controllers/ReportsController.js
+++ controllers/ReportsController.js
@@ -2,51 +2,64 @@ const Report = require('../models/Report');
 
 module.exports = {
 
-    renderCreate: (req, res) => {
-        return res.render('report/new', {PageTitle: "Create new report"});
-      },
-
-    renderUpdate: async (req, res) => {
-        try {
-          const report = await Report.findById(req.params.id)
-          return res.render('report/update', {PageTitle: "Update report", report});
-        } catch (error) {
-          return next(error)
-        }
-      },
-
-    findAll: async (req, res) => {
-        try {
-            const reports = await Report.find();
-        return res.render('report/all-reports', { reports, PageTitle: "All Reports" })
-        } catch (error) {
-            return res.render('report/error', {error: error.message})
-        }
+    renderCreate: (req, res) =>
+    {
+        return res.render('report/new', { PageTitle: "Create new report" });
     },
 
-    findById: async (req, res, next) => {
-
-        const { id } = req.params;
-
-        try {
-            const report = await Report.findById(id);
-            if(!report) return res.redirect('/')
-            return res.render('report/single', {report: report, PageTitle: `ID: ${report.id}`})
-        } catch (error) {
+    renderUpdate: async (req, res) =>
+    {
+        try
+        {
+            const report = await Report.findById(req.params.id)
+            return res.render('report/update', { PageTitle: "Update report", report });
+        } catch (error)
+        {
             return next(error)
         }
     },
 
-    create: async (req, res, next) => {
+    findAll: async (req, res) =>
+    {
+        try
+        {
+            const reports = await Report.find();
+            return res.render('report/all-reports', { reports, PageTitle: "All Reports" })
+        } catch (error)
+        {
+            return res.render('report/error', { error: error.message })
+        }
+    },
 
-        try {
+    findById: async (req, res, next) =>
+    {
+
+        const { id } = req.params;
+
+        try
+        {
+            const report = await Report.findById(id);
+            if (!report) return res.redirect('/')
+            return res.render('report/single', { report: report, PageTitle: `ID: ${report.id}` })
+        } catch (error)
+        {
+            return next(error)
+        }
+    },
+
+    create: async (req, res, next) =>
+    {
+
+        try
+        {
             /**
              * @todo Validate prescence of request' body
              */
 
-            const created = await Report.create({title: req.body.title, body: req.body.body, author: req.oidc.user.nickname})
+            const created = await Report.create({ title: req.body.title, body: req.body.body, author: req.oidc.user.nickname })
             return res.redirect('/reports')
-        } catch (error) {
+        } catch (error)
+        {
             return next(error)
         }
     },
@@ -56,34 +69,45 @@ module.exports = {
      * @todo Validate request body
      * @todo Add middleware for securing resources that do not belong to other user.
      */
-    update: async (req, res, next) => {
-
-         const { id } = req.params;
-         
-         try {
-             const updated = await Report.findByIdAndUpdate(id, {$set: req.body})
-             if(updated.modifiedCount != 0) return res.redirect('/reports')
-
-             return res.redirect(`/reports/${updated.id}`)
-
-         } catch (error) {
-            return next(error)
-         }
-    },
-
-    delete: async (req, res, next) => {
+    update: async (req, res, next) =>
+    {
 
         const { id } = req.params;
-        
-        try {
+
+        try
+        {
+            const updated = await Report.findByIdAndUpdate(id, { $set: req.body })
+            if (updated.modifiedCount != 0)
+            {
+                let error = { message: 'Something happened and no reports were updated' }
+                return next(error)
+            }
+
+            return res.redirect(`/reports/${updated.id}`)
+
+        } catch (error)
+        {
+            return next(error)
+        }
+    },
+
+    delete: async (req, res, next) =>
+    {
+
+        const { id } = req.params;
+
+        try
+        {
             const report = await Report.findById(id)
-            if(!report) {
-                const error = {message: `No report found with id ${id}`}
+            if (!report)
+            {
+                const error = { message: `No report found with id ${id}` }
                 next(error)
             }
             const deleted = await Report.findByIdAndDelete(id);
             return res.status(200).redirect('/reports')
-        } catch (error) {
+        } catch (error)
+        {
             return next(error)
         }
     }
