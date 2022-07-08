fd4b3088956a3bd7eebaed4c4f469dae211bdddf
Diego Matheus <diegoamatheus@gmail.com>
2022-07-08 13:57:11 -0400

    successfully call auth0 api

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -1,3 +1,5 @@
+const axios = require('axios')
+
 module.exports = {
 
     renderLandingPage: (req, res) =>
@@ -19,8 +21,36 @@ module.exports = {
     {
         try
         {
-            // Call Auth0 API v2 for sending the user JSON data
-            res.render('user/profile', { PageTitle: "My Profile" })
+            // Get token response from the auth api
+            const token = await axios.post(process.env.tokenUrl,
+                {
+                    client_id: process.env.token_client_id,
+                    client_secret: process.env.token_client_secret,
+                    audience: process.env.audience,
+                    grant_type: process.env.grant_type
+                },
+
+                {
+                    headers: { 'content-type': 'application/json' }
+                })
+            // Get the token and access type from the response
+            const { access_token, token_type } = token.data;
+
+
+            // Set headers for calling the API
+            const options = { headers: { 'Authorization': `${token_type} ${access_token}` } }
+            const url = process.env.audience + 'users';
+
+            // Get user profile from Auth0 API
+            const userProfile = await axios
+                .get(url, options)
+            const users = userProfile.data;
+            console.log('Users:', users)
+            // Call Auth0 Management API to get the access token.
+            // Call Auth0 API and send the access token and retrieve user profile
+            // Review the received data to decide what will be sent to the view.
+            // Sending the user JSON data
+            res.render('user/profile', { PageTitle: "My Profile", users })
         } catch (error)
         {
             next(error)
