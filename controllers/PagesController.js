a1429ad0c4b47d8ec3541f0074826eb80870fb0b
Diego Matheus <diegoamatheus@gmail.com>
2022-07-08 14:34:34 -0400

    success calling user profile from auth0 api

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -39,15 +39,16 @@ module.exports = {
 
             // Set headers for calling the API
             const options = { headers: { 'Authorization': `${token_type} ${access_token}` } }
-            const url = process.env.audience + 'users';
-
+            const url = process.env.audience + `users-by-email?email=${req.oidc.user.email}`;
             // Get user profile from Auth0 API - Add /{id} to the url to call for one specific user.
             // url = `${url}{req.oidc.user.id}`
             const response = await axios
                 .get(url, options)
-            const users = response.data;
-
-            res.render('user/profile', { PageTitle: "My Profile", users })
+            const profile = response.data;
+            console.log(profile)
+            // Users can have more than one profile (OAuth, Auth0, Github)
+            // @todo Write hbs helper for checking if there is more than one profile
+            res.render('user/profile', { PageTitle: "My Profile", profile })
         } catch (error)
         {
             next(error)
