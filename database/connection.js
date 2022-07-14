49023a9193a7bd774ecdddceb988f64068f6b970
Diego Matheus <diegoamatheus@gmail.com>
2022-07-14 12:08:25 -0400

    changes styles in all reports page

--- database/connection.js
+++ database/connection.js
@@ -3,16 +3,21 @@ const Mongoose = require("mongoose");
 const connection_url = process.env.prod_db_uri;
 module.exports = () =>
 {
-    Mongoose.connect(connection_url)
-        .then((db) =>
-        {
-            console.log("database connection ok")
-        })
-        .catch((error) =>
-        {
-            // Throw the error || Handle the error
-            console.log('Error:', error.message)
-        });
+    Mongoose.connect(connection_url, (error) =>
+    {
+        if (error) console.log(error)
+
+        console.log("Database connection ok")
+    })
+    // .then((db) =>
+    // {
+    //     console.log("database connection ok")
+    // })
+    // .catch((error) =>
+    // {
+    //     // Throw the error || Handle the error
+    //     console.log('Error:', error.message)
+    // });
 
 }
 
